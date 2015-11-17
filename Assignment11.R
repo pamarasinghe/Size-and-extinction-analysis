3494050b55b1f7235d86ea9be358c6059bde7352
Prabha <pamarasinghe@ufl.edu>
2015-11-16 19:04:45 -0500

    find average mass of extinct and extant mammals

--- Assignment11.R
+++ Assignment11.R
@@ -8,3 +8,13 @@ mammal_sizes<-read.csv('mammal_sizes.txt',sep="\t",header = FALSE, stringsAsFact
 ##Add column names
 colnames(mammal_sizes) <- c("continent", "status", "order","family", "genus", "species", "log_mass", "combined_mass","reference")
 
+##Exercise1.2
+by_status <- group_by(mammal_sizes,status)
+avg_mass_var <- summarize(by_status,avg_mass= mean(na.omit(combined_mass)))
+average_mass_extinct <- avg_mass_var[avg_mass_var$status=="extinct",c('avg_mass')]
+average_mass_extinct$avg_mass
+
+average_mass_extant <- avg_mass_var[avg_mass_var$status=="extant",c('avg_mass')]
+average_mass_extant$avg_mass
+
+
